fcdec36e41dfa6f8b4c3ce0fba10e62797469567
raulgallardop <raulgallardoperez@gmail.com>
2022-07-15 13:54:24 +0200

    creating and comparing roles

--- flask/BL/BusinessLayer.py
+++ flask/BL/BusinessLayer.py
@@ -1,32 +1,107 @@
-from typing import List
+from __future__ import annotations
 from flask_wtf import FlaskForm
-from ..forms import PersonalityForm, RoleForm
+import json
+from os.path import exists
+
 
 class BusinessLayer:
-    __roles: List[dict]
+    __roles_test = []
     __personality_test = []
+    __data_manager: DataManager
 
-    def BusinessLayer(self):
-        self.__roles = []
-        self.__personality_test = []
-        print("BL created")
+    def __init__(self):
+        self.__data_manager = FileDataManager('data')
+        self.__personality_test, self.__roles_test = self.__data_manager.load_data()
 
-    def addPersonalityTest(self, test: FlaskForm):
 
-        test_result = {field.short_name: field.data for field in test
-                       if field.short_name != 'csrf_token'}
-        self.__personality_test.append(test_result)
-        print(self.__personality_test)
-
-    def addRoleTest(self, test: RoleForm):
+    def add_personality_test(self, test: FlaskForm):
 
         test_result = {field.short_name: field.data for field in test
                        if field.short_name != 'csrf_token'}
         self.__personality_test.append(test_result)
-        print(self.__personality_test)
+        self.__data_manager.save_data(self.__personality_test,
+                                      self.__roles_test)
 
-    def getPersonalityTest(self):
-        return self.__roles
 
-    def getQuestionsFromForm(self, test:FlaskForm):
-        return
+    def add_role_test(self, test: FlaskForm):
+
+        test_result = {field.short_name: field.data for field in test
+                       if field.short_name != 'csrf_token'}
+        self.__roles_test.append(test_result)
+        self.__data_manager.save_data(self.__personality_test,
+                                      self.__roles_test)
+
+
+    def get_personality_test(self):
+        return self.__personality_test
+
+    def get_roles_test(self):
+        return self.__roles_test
+
+    def get_calculate_distance(self, email: str):
+        user_personality_test = self.__get_user(email)
+        distance = []
+
+        for roles_test in self.__roles_test:
+            role_distances = []
+            for key, value in user_personality_test.items():
+                if key in roles_test.keys():
+                    role_distances.append(abs(value - roles_test[key]))
+
+            distance_value = -1
+            if len(role_distances) > 0:
+                distance_value = sum(role_distances) / len(role_distances)
+
+            distance.append((roles_test['rolename'], distance_value))
+        distance.sort(key=lambda tup: tup[1])
+        return distance
+
+    def __get_user(self, email: str):
+        personality_test = {}
+        for user in self.__personality_test:
+            if user['email'] == email:
+                personality_test = user
+        return personality_test
+
+
+class DataManager:
+    def save_data(self, personality_test, roles_test):
+        pass
+
+    def load_data(self):
+        pass
+
+
+class FileDataManager(DataManager):
+    def __init__(self, path: str):
+        self.__path = path
+
+    def save_data(self, personality_test, roles_test):
+        personality_json_object = json.dumps(personality_test, indent=4)
+        roles_json_object = json.dumps(roles_test, indent=4)
+
+        self.__save_file('personality_test', personality_json_object)
+        self.__save_file('roles_test', roles_json_object)
+
+    def __save_file(self, file_name:str, data):
+        file_path = self.__get_file_path(file_name)
+        with open(file_path, 'w') as outfile:
+            json.dump(data, outfile)
+
+    def load_data(self):
+        personality_test = self.__load_file('personality_test')
+        roles_test = self.__load_file('roles_test')
+
+        return personality_test, roles_test
+
+    def __load_file(self, file_name:str):
+        file_path = self.__get_file_path(file_name)
+        if exists(file_path):
+            with open(file_path) as json_file:
+                data = json.load(json_file)
+                return json.loads(data)
+
+        return []
+
+    def __get_file_path(self, file_name):
+        return self.__path + '/' + file_name + '.json'
--- flask/app.py
+++ flask/app.py
@@ -7,43 +7,52 @@ app.config['SECRET_KEY'] = 'your secret key'
 
 businesslayer = BusinessLayer()
 
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
     form = CandidatePersonalityForm()
-    if form.validate_on_submit():
 
-        businesslayer.addPersonalityTest(form)
-        return redirect(url_for('roles'))
+    if form.validate_on_submit():
+        businesslayer.add_personality_test(form)
+
+        return redirect(url_for('results', email=form.email.data))
+
     return render_template('index.html', form=form)
+
 
 @app.route('/roles/')
 def roles():
-    print("Roles entering")
     return render_template('roles.html',
-                           personality_list=businesslayer.getPersonalityTest())
+                           personality_list=businesslayer.get_roles_test())
 
-@app.route('/addroles/',methods=('GET', 'POST'))
+
+@app.route('/addroles/', methods=('GET', 'POST'))
 def add_roles():
     form = RoleForm()
-    print("before submit")
     if form.validate_on_submit():
-        print("enter")
-        businesslayer.addRolePersonalityTest(form)
+        businesslayer.add_role_test(form)
         return redirect(url_for('roles'))
-    else:
-        print({field.short_name: field.data for field in form
-         if field.short_name != 'csrf_token'})
-    return render_template('addroles.html', personality_list=businesslayer.getPersonalityTest(),
+
+    return render_template('addroles.html', personality_list=businesslayer.get_personality_test(),
                            form=form)
+
 
 @app.route('/test')
 def test_connection():
     return '<h1>test</h2>'
 
+
+@app.route('/results/<email>')
+def results(email):
+    distances = businesslayer.get_calculate_distance(email)
+    return render_template('results.html', distances=distances)
+
+
 @app.route('/questions/')
 def questions():
     form = RoleForm()
     return render_template('questions.html', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
